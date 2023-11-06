7a23c75eb9bfa70e73c55703be3a85f7520a41f2
Hang Phung <hang.phungthu01@gmail.com>
2023-11-06 16:50:16 +0700

    Fix Vehicle Download file

--- benchmark/vehicle_classification/download.sh
+++ benchmark/vehicle_classification/download.sh
@@ -1,8 +1,8 @@
 # Vehicle dataset
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=14Qq9aDeKIIM0hASB-RcU_A6g8PhuhnGZ' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=14Qq9aDeKIIM0hASB-RcU_A6g8PhuhnGZ" -O ./benchmark/RAW_DATA/VEHICLE/x_train.npy && rm -rf /tmp/cookies.txt
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=14Qq9aDeKIIM0hASB-RcU_A6g8PhuhnGZ' -O ./benchmark/RAW_DATA/VEHICLE/x_train.npy
 
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1zbWwXiROogxC3bkY7hNYSyyBPkY3J8Y4' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1zbWwXiROogxC3bkY7hNYSyyBPkY3J8Y4" -O ./benchmark/RAW_DATA/VEHICLE/x_test.npy && rm -rf /tmp/cookies.txt
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1zbWwXiROogxC3bkY7hNYSyyBPkY3J8Y4' -O ./benchmark/RAW_DATA/VEHICLE/x_test.npy
 
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1oXckPgcKLR4wDU3gcYbcq8mvv35co6n0' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1oXckPgcKLR4wDU3gcYbcq8mvv35co6n0" -O ./benchmark/RAW_DATA/VEHICLE/y_train.npy && rm -rf /tmp/cookies.txt
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1oXckPgcKLR4wDU3gcYbcq8mvv35co6n0' -O ./benchmark/RAW_DATA/VEHICLE/y_train.npy
 
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1lY-CY6bP28-3M0bQ38xaCzuZlHLD-tLY' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1lY-CY6bP28-3M0bQ38xaCzuZlHLD-tLY" -O ./benchmark/RAW_DATA/VEHICLE/y_test.npy && rm -rf /tmp/cookies.txt
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1lY-CY6bP28-3M0bQ38xaCzuZlHLD-tLY' -O ./benchmark/RAW_DATA/VEHICLE/y_test.npy
