Fix GBlend calculation in ECG

algorithm/multimodal/ptbxl_classification/mifl.py
@@ -126,7 +126,7 @@ class Server(BasicServer):
             att_mat = torch.softmax(params.matmul(params.T) / np.sqrt(dim), dim=1)
             for idk, k in enumerate(modal_dict[m]):
                 for idl, l in enumerate(modal_dict[m]):
-                    A[m, k, l] = att_mat[idk, idl]
+                    A[m, k, l] = att_mat[idk, idl]  
         # classifier
         params = torch.stack([
             torch.cat([

benchmark/ptbxl_classification/core.py
@@ -365,7 +365,8 @@ class TaskCalculator(ClassificationCalculator):
                 total_loss = loss + total_loss
             
         
-        loss_eval = [loss / (batch_id + 1) for loss in total_loss]
+        # loss_eval = [loss / (batch_id + 1) for loss in total_loss]
+        loss_eval = [loss for loss in total_loss]
         
         #     total_loss += loss.item() * len(batch_data[-1])
         #     predicts.extend(torch.argmax(torch.softmax(outputs, dim=1), dim=1).cpu().tolist())
@@ -406,6 +407,7 @@ class TaskCalculator(ClassificationCalculator):
                 #     loss_each_modal[i] += loss_leads[i] * len(batch_data[-1])
                 total_loss += loss.item() * len(batch_data[-1])
                 predicts.extend(torch.argmax(torch.softmax(outputs, dim=1), dim=1).cpu().tolist())
+            # import pdb; pdb.set_trace()
             # import pdb; pdb.set_trace()
             labels = np.array(labels)
             predicts = np.array(predicts)

benchmark/ptbxl_classification/model/mifl_gblend.py
@@ -145,8 +145,8 @@ class Model(FModule):
         for i in range(self.n_leads):
             self.feature_extractors.append(Inception1DBase(input_channels=1))
             self.relation_embedders.append(RelationEmbedder())
-            self.classifiers.append(Classifier())
-        self.multi_classifier = Multi_Classifier()
+            self.classifiers.append(Classifier())   # classifier for each modality
+        self.multi_classifier = Multi_Classifier()  # classifier for multimodal
         self.criterion = nn.CrossEntropyLoss()
         
     def forward(self, x, y, leads):