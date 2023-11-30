c380a451a3a189f9305d1095b50e481fab3bdc3a
Hang Phung <hang.phungthu01@gmail.com>
2023-11-30 09:08:22 +0700

    Add IEMOCAP 4 classes

--- benchmark/iemocap_cogmen_classification/download.sh
+++ benchmark/iemocap_cogmen_classification/download.sh
@@ -1,8 +1,18 @@
-# IEMOCAP_COGMEN dataset
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1BAxa0IBtN-h2pbTVsMRII5cw9ebID_l-' -O ./benchmark/RAW_DATA/IEMOCAP_COGMEN/x_train.pt
+# IEMOCAP_COGMEN_6 dataset
+# wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1BAxa0IBtN-h2pbTVsMRII5cw9ebID_l-' -O ./benchmark/RAW_DATA/IEMOCAP_COGMEN/x_train.pt
 
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1SKJdjGelZ9yvR-6GBtl5KRGmtdOy8T4F' -O ./benchmark/RAW_DATA/IEMOCAP_COGMEN/x_test.pt
+# wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1SKJdjGelZ9yvR-6GBtl5KRGmtdOy8T4F' -O ./benchmark/RAW_DATA/IEMOCAP_COGMEN/x_test.pt
 
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=14kmtGHcOKjyOg17GVmnRPzXlyJUnwl1F' -O ./benchmark/RAW_DATA/IEMOCAP_COGMEN/y_train.pt
+# wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=14kmtGHcOKjyOg17GVmnRPzXlyJUnwl1F' -O ./benchmark/RAW_DATA/IEMOCAP_COGMEN/y_train.pt
 
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1QFPAlfBEDhBJwI_A_6SX1lV7Qs09bXTm' -O ./benchmark/RAW_DATA/IEMOCAP_COGMEN/y_test.pt
+# wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1QFPAlfBEDhBJwI_A_6SX1lV7Qs09bXTm' -O ./benchmark/RAW_DATA/IEMOCAP_COGMEN/y_test.pt
+
+
+# IEMOCAP_COGMEN_4 dataset
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1gxaKIuEDMF4HN9vGGt5mEjuk78OLKTqV' -O ./benchmark/RAW_DATA/IEMOCAP_COGMEN/x_train.pt
+
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=13sJ_XZ_OzCN5pqtZvkwIX4lf6bmdle7U' -O ./benchmark/RAW_DATA/IEMOCAP_COGMEN/x_test.pt
+
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1pv36PHrEMYAKodna6A_8USVDuIMYncDr' -O ./benchmark/RAW_DATA/IEMOCAP_COGMEN/y_train.pt
+
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1FRtC94pH0y8GBTmxavL0BcLPNl8ucwlb' -O ./benchmark/RAW_DATA/IEMOCAP_COGMEN/y_test.pt
