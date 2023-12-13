408b580d60a5e91c3ae6982905a956bf596b7b73
Hang Phung <hang.phungthu01@gmail.com>
2023-12-14 05:58:47 +0700

    Add scripts UCI-HAR

---
+++ script/ucihar/iid/missing_rate_0.3/mifl_0.1_cw0.5_batch64.sh
@@ -0,0 +1,20 @@
+python main_w_contrastive.py \
+    --task ucihar_classification_cnum20_dist0_skew0_seed0_missing_rate_0.3 \
+    --model mifl \
+    --algorithm multimodal.ucihar_classification.mifl \
+    --sample full \
+    --aggregate other \
+    --num_rounds 1000 \
+    --early_stop 50  \
+    --proportion 1.0 \
+    --lr_scheduler 0 \
+    --seed 1234 \
+    --fedmsplit_prox_lambda 0.01 \
+    --contrastive_weight 0.5 \
+    --learning_rate 0.1 \
+    --num_epochs 3 \
+    --learning_rate_decay 1.0 \
+    --batch_size 64 \
+    --test_batch_size 64 \
+    --gpu 1 \
+    --wandb
