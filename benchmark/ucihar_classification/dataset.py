d225ae4e7be24c21d47b78e2f8c2ac8bcc3c033d
Hang Phung <hang.phungthu01@gmail.com>
2023-12-13 10:25:46 +0700

    Add UCI-HAR Dataset

---
+++ benchmark/ucihar_classification/dataset.py
@@ -0,0 +1,53 @@
+from torch.utils.data import Dataset
+from sentence_transformers import SentenceTransformer
+import numpy as np
+import os
+import torch
+import pickle
+import random
+
+class UCIHARDataset(Dataset):
+    def __init__(self, root, download=True, standard_scaler=False, train=True, crop_length=0):
+        self.root = root
+        self.standard_scaler = standard_scaler
+        self.train = train
+        self.crop_length = crop_length
+        # import pdb; pdb.set_trace()
+        if not os.path.exists(self.root):
+            if download:
+                print('Downloading UCIHAR Dataset...', end=' ')
+                # os.makedirs(root, exist_ok=True)
+                os.system('bash ./benchmark/ucihar_classification/download.sh')
+                print('done!')
+            else:
+                raise RuntimeError('Dataset not found. You can use download=True to download it')
+        
+        
+        if self.train:
+            # import pdb; pdb.set_trace()
+            self.x = np.load(os.path.join(self.root, 'x_train.npy'), allow_pickle=True)
+            self.y = np.load(os.path.join(self.root, 'y_train.npy'), allow_pickle=True)
+            # self.y = np.array(self.y.reshape(-1), dtype='int64')
+            # import pdb; pdb.set_trace()
+        
+        else:
+            self.x = np.load(os.path.join(self.root, 'x_test.npy'), allow_pickle=True)
+            self.y = np.load(os.path.join(self.root, 'y_test.npy'), allow_pickle=True)
+            # self.y = np.array(self.y.reshape(-1), dtype='int64')
+
+    
+    def __len__(self):
+        return self.y.shape[0]
+    
+    def __getitem__(self, index):
+        x = self.x[index]
+        y = self.y[index]
+        # start_idx = random.randint(0, x.shape[0] - self.crop_length - 1)
+        # x = x[start_idx:start_idx + self.crop_length].transpose().astype(np.float32)
+        # x = np.transpose(x,(1,0)).reshape(2,50).astype(np.float32)
+        # y = y.astype(np.float32)
+        # y = int(y)
+        return x, y
+    
+if __name__ == '__main__':
+    dataset = UCIHARDataset(root='./benchmark/RAW_DATA/UCIHAR', standard_scaler=False, train=True)
---
+++ benchmark/ucihar_classification/download.sh
@@ -0,0 +1,6 @@
+cd ./benchmark/RAW_DATA
+
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/00240/UCI%20HAR%20Dataset.zip
+unzip UCI\ HAR\ Dataset.zip
+mv UCI\ HAR\ Dataset UCIHAR
+rm UCI\ HAR\ Dataset.zip && rm -r __MACOSX
