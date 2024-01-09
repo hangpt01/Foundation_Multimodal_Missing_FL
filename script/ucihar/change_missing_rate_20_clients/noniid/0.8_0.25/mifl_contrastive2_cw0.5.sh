576fbe31471283573ef380eaf1e42612cd89d24c
Hang Phung <hang.phungthu01@gmail.com>
2024-01-09 10:03:55 +0700

    Add contrastive2 - lr0.1 UCIHAR scripts

---
+++ script/ucihar/change_missing_rate_20_clients/noniid/0.8_0.25/mifl_contrastive2_cw0.5.sh
@@ -0,0 +1,20 @@
+python main.py \
+    --task ucihar_classification_cnum20_dist1_skew0.5_seed0_missing_rate_0.8_miss_ratio_0.25 \
+    --model mifl_contrastive2 \
+    --algorithm multimodal.ucihar_classification.mifl \
+    --sample full \
+    --aggregate other \
+    --num_rounds 1000 \
+    --early_stop 50  \
+    --proportion 1.0 \
+    --lr_scheduler 0 \
+    --seed 1234 \
+    --fedmsplit_prox_lambda 0.01 \
+    --contrastive_weight 0.5 \
+    --learning_rate 0.1 \
+    --num_epochs 3 \
+    --learning_rate_decay 1.0 \
+    --batch_size 128 \
+    --test_batch_size 128 \
+    --gpu 1 \
+    --wandb
