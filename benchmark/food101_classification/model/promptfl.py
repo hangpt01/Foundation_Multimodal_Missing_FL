35448224942a4b5138135459988394a9d1d1fa45
Hang Phung <hang.phungthu01@gmail.com>
2024-03-21 02:04:38 +0700

    Creating PromptFL

--- benchmark/food101_classification/model/promptfl.py
+++ benchmark/food101_classification/model/promptfl.py
@@ -2,7 +2,7 @@ from typing import Any
 import torch
 from torch import nn
 import torch.nn.functional as F
-from transformers import ViltModel, ViltProcessor
+from transformers import ViltModel, ViltProcessor, BertTokenizerFast
 import numpy as np
 from utils.fmodule import FModule
 
@@ -19,34 +19,37 @@ class PromptLearner(FModule):
     def __init__(self, num_classes=3):
         super().__init__()
         
-        num_context_vectors = 16
+        self.num_context_vectors = 16
         # class-specific context vectors
-        ctx_vectors = torch.empty(num_classes, num_context_vectors, 40)
+        ctx_vectors = torch.empty(num_classes, self.num_context_vectors, 40)
         nn.init.normal_(ctx_vectors, std=0.02)
         self.prompt_learner = nn.Parameter(ctx_vectors)
         
-        prompt_prefix = " ".join(["X"] * num_context_vectors)
+        prompt_prefix = " ".join(["X"] * self.num_context_vectors)
         print(f'Initial context: "{prompt_prefix}"')
-        print(f"Number of context words (tokens): {num_context_vectors}")
+        print(f"Number of context words (tokens): {self.num_context_vectors}")
 
         self.ctx = nn.Parameter(ctx_vectors)  # to be optimized
 
+
+        classnames = ["apple_pie", "baby_back_ribs" , "baklava"]
+        _tokenizer = BertTokenizerFast.from_pretrained("bert-base-uncased")
         classnames = [name.replace("_", " ") for name in classnames]
         name_lens = [len(_tokenizer.encode(name)) for name in classnames]
         prompts = [prompt_prefix + " " + name + "." for name in classnames]
 
-        tokenized_prompts = torch.cat([clip.tokenize(p) for p in prompts])
-        with torch.no_grad():
-            embedding = clip_model.token_embedding(tokenized_prompts).type(dtype)
+        tokenized_prompts = torch.cat([_tokenizer.tokenize(p) for p in prompts])
+        
+        # with torch.no_grad():
+        #     embedding = clip_model.token_embedding(tokenized_prompts).type(dtype)
 
         # These token vectors will be saved when in save_model(),
         # but they should be ignored in load_model() as we want to use
         # those computed using the current class names
-        self.register_buffer("token_prefix", embedding[:, :1, :])  # SOS
-        self.register_buffer("token_suffix", embedding[:, 1 + n_ctx :, :])  # CLS, EOS
+        # self.register_buffer("token_prefix", embedding[:, :1, :])  # SOS
+        # self.register_buffer("token_suffix", embedding[:, 1 + n_ctx :, :])  # CLS, EOS
 
         self.num_classes = num_classes
-        self.n_ctx = n_ctx
         self.tokenized_prompts = tokenized_prompts  # torch.Tensor
         self.name_lens = name_lens
 
@@ -70,6 +73,9 @@ class PromptLearner(FModule):
         )
 
 
+        # concat to text
+
+
         return prompts
 
 
@@ -91,16 +97,17 @@ class Model(FModule):
         super(Model, self).__init__()
         self.n_leads = 2
         self.hidden_size = 768
-        
-        # self.backbone = ViltModel.from_pretrained("dandelin/vilt-b32-mlm")
-        # for param in self.backbone.parameters():
-        #     param.requires_grad = False
-            
+    
+        self.prompt_learner = PromptLearner()
+        self.tokenized_prompts = self.prompt_learner.tokenized_prompts
+
         self.classifier = Classifier()
         self.criterion = nn.CrossEntropyLoss()
         
     def forward(self, backbone, batch, labels, leads):
         # import pdb; pdb.set_trace()
+        prompts = self.prompt_learner()
+        import pdb; pdb.set_trace()
         missing_batch = dict()
         batch_size = labels.shape[0]
         device = labels.device
