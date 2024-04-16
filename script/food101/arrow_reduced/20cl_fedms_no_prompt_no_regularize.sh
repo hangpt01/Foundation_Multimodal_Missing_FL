cad91eb6f3e12112bc8c468737c96200d0d2abdf
Hang Phung <hang.phungthu01@gmail.com>
2024-04-16 15:32:50 +0700

    Add FedMSplit-no-prompt with no regularization

---
+++ script/food101/arrow_reduced/20cl_fedms_no_prompt_no_regularize.sh
@@ -0,0 +1,19 @@
+python main.py \
+    --task food101_classification_arrow_reduced_cnum20_dist0_skew0_seed0_missing_each_0.25 \
+    --model fedmsplit_no_prompt_no_regularize \
+    --algorithm multimodal.food101_classification_arrow_reduced.fedmsplit_no_prompt_no_regularize \
+    --sample full \
+    --aggregate other \
+    --num_rounds 1000 \
+    --early_stop 30  \
+    --proportion 1.0 \
+    --lr_scheduler 0 \
+    --seed 1234 \
+    --fedmsplit_prox_lambda 0.1 \
+    --learning_rate 0.1 \
+    --num_epochs 1 \
+    --learning_rate_decay 1.0 \
+    --batch_size 64 \
+    --test_batch_size 64 \
+    --gpu 0 \
+    --wandb
