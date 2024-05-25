24ce61d847a5ef39dc20e18db8b23d156af14791
Hang Phung <hang.phungthu01@gmail.com>
2024-05-26 01:19:46 +0700

    Change centralized scripts

--- script/food101/8_classes/noniid_0.1/centralized_no_prompt.sh
+++ script/food101/8_classes/noniid_0.1/centralized_no_prompt.sh
@@ -1,3 +1,13 @@
+python generate_fedtask.py \
+    --benchmark food101_classification_8_classes \
+    --dist 1 \
+    --skew 0.1 \
+    --num_clients 1 \
+    --seed 0 \
+    --missing \
+    --missing_ratio_train 0.7 \
+    --missing_ratio_test 0.7 \
+
 python main.py \
     --task food101_classification_8_classes_cnum1_dist1_skew0.1_seed0_missing_ratio_0.7_0.7_missing_type_both_both_both_ratio_0.5 \
     --model centralized_no_prompt \
@@ -10,9 +20,9 @@ python main.py \
     --lr_scheduler 0 \
     --seed 1234 \
     --fedmsplit_prox_lambda 0 \
-    --learning_rate 0.5 \
+    --learning_rate 0.01 \
     --num_epochs 1 \
-    --learning_rate_decay 0.99 \
+    --learning_rate_decay 1 \
     --batch_size 64 \
     --test_batch_size 64 \
     --gpu 1 \
