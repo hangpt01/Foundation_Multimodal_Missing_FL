606ac5175e48e288069fc673cb8f37070b54f44b
Hang Phung <91205917+hangpt01@users.noreply.github.com>
2024-08-22 21:56:05 +0700

    Update 20cl_prob_prompt.sh

--- script/food101/8_classes/image_1_text_0.3/iid/20cl_prob_prompt.sh
+++ script/food101/8_classes/image_1_text_0.3/iid/20cl_prob_prompt.sh
@@ -5,12 +5,12 @@ python main.py \
     --sample full \
     --aggregate other \
     --num_rounds 1000 \
-    --early_stop 30  \
+    --early_stop 50  \
     --proportion 1.0 \
     --lr_scheduler 0 \
     --seed 1234 \
     --fedmsplit_prox_lambda 0 \
-    --learning_rate 0.1 \
+    --learning_rate 0.05 \
     --num_epochs 1 \
     --learning_rate_decay 1.0 \
     --batch_size 512 \
