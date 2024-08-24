868cbdfffd8a11077bfc33e2c2f813903aba3eb0
Hang Phung <hang.phungthu01@gmail.com>
2024-08-24 10:53:07 +0700

    Take back the old L2P model

--- script/food101/8_classes/image_1_text_0.3/iid/missing_data.sh
+++ script/food101/8_classes/image_1_text_0.3/iid/missing_data.sh
@@ -1,20 +1,18 @@
 python main.py \
     --task food101_classification_8_classes_cnum20_dist0_skew0_seed0_missing_ratio_0.7_0.7_missing_type_text_text_both_ratio_0.0 \
-    --model probabilistic_prompt_full \
-    --algorithm multimodal.food101_classification_8_classes.prob_prompt \
+    --model get_missing_data \
+    --algorithm multimodal.food101_classification_8_classes.get_missing_data \
     --sample full \
     --aggregate other \
     --num_rounds 1000 \
-    --early_stop 50  \
+    --early_stop 30  \
     --proportion 1.0 \
     --lr_scheduler 0 \
     --seed 1234 \
-    --fedmsplit_prox_lambda 0 \
+    --fedmsplit_prox_lambda 0.1 \
     --learning_rate 0.05 \
     --num_epochs 1 \
     --learning_rate_decay 1.0 \
-    --batch_size 512 \
-    --test_batch_size 512 \
-    --gpu 0 
-    # \
-    # --wandb
+    --batch_size 64 \
+    --test_batch_size 64 \
+    --gpu 0
