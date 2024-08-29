c819ffc015b8ddf19bce1ec168b1d7351cce23f5
Hang Phung <hang.phungthu01@gmail.com>
2024-08-29 10:34:55 +0700

    Add centralized_prompt script

---
+++ script/food101/8_classes/image_1_text_0.3/iid/centralized_prompt.sh
@@ -0,0 +1,19 @@
+python main.py \
+    --task food101_classification_8_classes_cnum1_dist0_skew0_seed0_missing_ratio_0.7_0.7_missing_type_text_text_both_ratio_0.0 \
+    --model centralized_prompt \
+    --algorithm multimodal.food101_classification_8_classes.centralized_prompt \
+    --sample full \
+    --aggregate other \
+    --num_rounds 1000 \
+    --early_stop 50  \
+    --proportion 1.0 \
+    --lr_scheduler 0 \
+    --seed 1234 \
+    --fedmsplit_prox_lambda 0 \
+    --learning_rate 0.05 \
+    --num_epochs 1 \
+    --learning_rate_decay 1.0 \
+    --batch_size 4096 \
+    --test_batch_size 4096 \
+    --gpu 0 \
+    --wandb
