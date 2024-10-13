035a55d5da7fd24efe520b7bf83301e7ac8501b2
Hang Phung <hang.phungthu01@gmail.com>
2024-10-13 15:42:11 +0700

    Add scripts Agglo_mean text256

--- script/food101/8_classes/image_1_text_0.3/iid_no_earlystop/Agglo_mean_text256.sh
+++ script/food101/8_classes/image_1_text_0.3/iid_no_earlystop/Agglo_mean_text256.sh
@@ -1,7 +1,7 @@
 python main.py \
     --task food101_classification_8_classes_cnum20_dist0_skew0_seed0_missing_ratio_0.7_0.7_missing_type_text_text_both_ratio_0.0 \
-    --model L2P_Prob_prompt \
-    --algorithm multimodal.food101_classification_8_classes.L2P_Prob_prompt \
+    --model Agglo_mean \
+    --algorithm multimodal.food101_classification_8_classes.Agglo_mean \
     --sample full \
     --aggregate other \
     --num_rounds 300 \
@@ -13,7 +13,7 @@ python main.py \
     --num_epochs 1 \
     --num_outer_loops 5 \
     --learning_rate_decay 1.0 \
-    --note 101cls_loadvilt \
+    --note 3clusters_loadvilt \
     --batch_size 512 \
     --test_batch_size 512 \
     --max_text_len 256 \
