4b545813cfe2493cc0ac13f474916d9f3adc7c97
Hang Phung <hang.phungthu01@gmail.com>
2024-10-17 01:22:28 +0700

    Add nonIID 0.5 Food

---
+++ script/food101/8_classes/image_1_text_0.3/noniid0.5/1clients.sh
@@ -0,0 +1,13 @@
+python generate_fedtask.py \
+    --benchmark food101_classification_8_classes \
+    --dist 1 \
+    --skew 0.5 \
+    --num_clients 1 \
+    --seed 0 \
+    --missing \
+    --missing_ratio_train 0.7 \
+    --missing_ratio_test 0.7 \
+    --missing_type_train text \
+    --missing_type_test text \
+    --both_ratio 0 \
+    --max_text_len 40
---
+++ script/food101/8_classes/image_1_text_0.3/noniid0.5/20cl_L2P.sh
@@ -0,0 +1,20 @@
+python main.py \
+    --task food101_classification_8_classes_cnum20_dist1_skew0.5_seed0_missing_ratio_0.7_0.7_missing_type_text_text_both_ratio_0.0 \
+    --model L2P \
+    --algorithm multimodal.food101_classification_8_classes.L2P \
+    --sample full \
+    --aggregate other \
+    --num_rounds 250 \
+    --proportion 1.0 \
+    --lr_scheduler 0 \
+    --seed 1234 \
+    --fedmsplit_prox_lambda 0 \
+    --learning_rate 0.05 \
+    --num_epochs 1 \
+    --learning_rate_decay 1.0 \
+    --note size_20_topk_10 \
+    --batch_size 512 \
+    --test_batch_size 512 \
+    --max_text_len 40 \
+    --gpu 0 \
+    --wandb
