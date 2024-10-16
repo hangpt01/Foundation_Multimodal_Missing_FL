058638b4ffa10c17bf2b5be3643321214626db05
Hang Phung <hang.phungthu01@gmail.com>
2024-10-16 15:00:47 +0700

    Add NonIID 0.5 IMDB

---
+++ script/imdb/noniid0.5/20cl_L2P.sh
@@ -0,0 +1,20 @@
+python main.py \
+    --task imdb_classification_cnum20_dist1_skew0.5_seed0_missing_ratio_0.7_0.7_missing_type_text_text_both_ratio_0.0 \
+    --model L2P \
+    --algorithm multimodal.imdb_classification.L2P \
+    --sample full \
+    --aggregate other \
+    --num_rounds 250 \
+    --proportion 1.0 \
+    --lr_scheduler 0 \
+    --seed 1234 \
+    --fedmsplit_prox_lambda 0 \
+    --learning_rate 0.01 \
+    --num_epochs 1 \
+    --learning_rate_decay 1.0 \
+    --note loadvilt \
+    --batch_size 512 \
+    --test_batch_size 512 \
+    --max_text_len 128 \
+    --gpu 0 \
+    --wandb
