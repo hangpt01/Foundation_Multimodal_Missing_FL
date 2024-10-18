6cdaffa8d6f01673c8ff7c9ce75a6c9b83bbcc1b
Hang Phung <hang.phungthu01@gmail.com>
2024-10-18 10:22:42 +0700

    NonIID 0.9 Food to test

---
+++ script/imdb/img0.3_full0.7/noniid0.5/20cl_L2P_Prob_prompt.sh
@@ -0,0 +1,21 @@
+python main.py \
+    --task imdb_classification_cnum20_dist1_skew0.5_seed0_missing_ratio_0.7_0.7_missing_type_text_text_both_ratio_0.0 \
+    --model L2P_Prob_prompt \
+    --algorithm multimodal.imdb_classification.L2P_Prob_prompt \
+    --sample full \
+    --aggregate other \
+    --num_rounds 250 \
+    --proportion 1.0 \
+    --lr_scheduler 0 \
+    --seed 1234 \
+    --fedmsplit_prox_lambda 0 \
+    --learning_rate 0.01 \
+    --num_epochs 1 \
+    --num_outer_loops 5 \
+    --learning_rate_decay 1.0 \
+    --note loadvilt \
+    --batch_size 256 \
+    --test_batch_size 256 \
+    --max_text_len 128 \
+    --gpu 0 \
+    --wandb
