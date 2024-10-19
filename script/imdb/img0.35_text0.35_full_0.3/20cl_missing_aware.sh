74086ed6e7c3b70081530c20ee72ae719cf75712
Hang Phung <hang.phungthu01@gmail.com>
2024-10-19 09:18:31 +0700

    Add scripts IMDB - miss_both 0.7

---
+++ script/imdb/img0.35_text0.35_full_0.3/20cl_missing_aware.sh
@@ -0,0 +1,20 @@
+python main.py \
+    --task imdb_classification_cnum20_dist0_skew0_seed0_missing_ratio_0.7_0.7_missing_type_both_both_both_ratio_0.5 \
+    --model missing_aware \
+    --algorithm multimodal.imdb_classification.missing_aware \
+    --sample full \
+    --aggregate other \
+    --num_rounds 250 \
+    --proportion 1.0 \
+    --lr_scheduler 0 \
+    --seed 1234 \
+    --fedmsplit_prox_lambda 0 \
+    --learning_rate 0.01 \
+    --num_epochs 1 \
+    --learning_rate_decay 1.0 \
+    --note loadvilt \
+    --batch_size 256 \
+    --test_batch_size 256 \
+    --max_text_len 128 \
+    --gpu 0 \
+    --wandb
