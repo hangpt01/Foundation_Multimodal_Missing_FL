9baa52be2de763f72e76ac62e90c18690c93e418
Hang Phung <hang.phungthu01@gmail.com>
2024-10-22 11:33:19 +0700

    Add scripts - IMDB

---
+++ script/food101/8_classes/both_0.7_0.5/20cl_L2P_Prob_prompt.sh
@@ -0,0 +1,21 @@
+python main.py \
+    --task food101_classification_8_classes_cnum20_dist0_skew0_seed0_missing_ratio_0.7_0.7_missing_type_both_both_both_ratio_0.5 \
+    --model L2P_Prob_prompt \
+    --algorithm multimodal.food101_classification_8_classes.L2P_Prob_prompt \
+    --sample full \
+    --aggregate other \
+    --num_rounds 250 \
+    --proportion 1.0 \
+    --lr_scheduler 0 \
+    --seed 1234 \
+    --fedmsplit_prox_lambda 0 \
+    --learning_rate 0.05 \
+    --num_epochs 1 \
+    --num_outer_loops 5 \
+    --learning_rate_decay 1.0 \
+    --note loadvilt \
+    --batch_size 512 \
+    --test_batch_size 512 \
+    --max_text_len 40 \
+    --gpu 0 \
+    --wandb
