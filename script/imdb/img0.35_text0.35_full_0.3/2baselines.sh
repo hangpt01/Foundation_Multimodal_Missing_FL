567238f2a947b98590dbc67233887357457a7de8
Hang Phung <hang.phungthu01@gmail.com>
2024-10-22 11:35:17 +0700

    Add scripts - IMDB

--- script/imdb/img0.35_text0.35_full_0.3/2baselines.sh
+++ script/imdb/img0.35_text0.35_full_0.3/2baselines.sh
@@ -34,8 +34,8 @@ python main.py \
     --wandb
 python main.py \
     --task imdb_classification_cnum20_dist0_skew0_seed0_missing_ratio_0.7_0.7_missing_type_both_both_both_ratio_0.5 \
-    --model L2P \
-    --algorithm multimodal.imdb_classification.L2P \
+    --model Prob_prompt \
+    --algorithm multimodal.imdb_classification.Prob_prompt \
     --sample full \
     --aggregate other \
     --num_rounds 250 \
@@ -45,6 +45,7 @@ python main.py \
     --fedmsplit_prox_lambda 0 \
     --learning_rate 0.01 \
     --num_epochs 1 \
+    --num_outer_loops 5 \
     --learning_rate_decay 1.0 \
     --note loadvilt \
     --batch_size 256 \
