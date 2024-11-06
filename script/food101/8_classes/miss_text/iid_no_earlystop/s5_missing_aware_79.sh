bd88689a709a6ed20e464ab18cb4446157f2d766
Hang Phung <hang.phungthu01@gmail.com>
2024-11-06 11:44:08 +0700

    Add scripts Food

---
+++ script/food101/8_classes/miss_text/iid_no_earlystop/s5_missing_aware_79.sh
@@ -0,0 +1,20 @@
+python main.py \
+    --task food101_classification_8_classes_cnum20_dist0_skew0_seed0_missing_ratio_0.7_0.7_missing_type_text_text_both_ratio_0.0 \
+    --model missing_aware \
+    --algorithm multimodal.food101_classification_8_classes.missing_aware \
+    --sample full \
+    --aggregate other \
+    --num_rounds 250 \
+    --proportion 1.0 \
+    --lr_scheduler 0 \
+    --seed 5679 \
+    --fedmsplit_prox_lambda 0 \
+    --learning_rate 0.05 \
+    --num_epochs 1 \
+    --learning_rate_decay 1.0 \
+    --note loadvilt \
+    --batch_size 512 \
+    --test_batch_size 512 \
+    --max_text_len 40 \
+    --gpu 0 \
+    --wandb
