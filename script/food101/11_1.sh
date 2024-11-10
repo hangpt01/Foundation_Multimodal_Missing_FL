bc99fa70d9bf359dfd77e1f869e2ccf8f43fa576
Hang Phung <hang.phungthu01@gmail.com>
2024-11-11 01:52:00 +0700

    Add script Food final

--- script/food101/11_1.sh
+++ script/food101/11_1.sh
@@ -2,30 +2,31 @@ python generate_fedtask.py \
     --benchmark food101_classification_8_classes \
     --dist 0 \
     --skew 0 \
-    --num_clients 1 \
+    --num_clients 20 \
     --seed 0 \
     --missing \
     --missing_ratio_train 0.7 \
     --missing_ratio_test 0.7 \
     --missing_type_train text \
     --missing_type_test text \
-    --both_ratio 0
+    --both_ratio 0 \
+    --max_text_len 40
 python main.py \
     --task food101_classification_8_classes_cnum20_dist0_skew0_seed0_missing_ratio_0.7_0.7_missing_type_text_text_both_ratio_0.0 \
-    --model L2P_Prob_prompt \
-    --algorithm multimodal.food101_classification_8_classes.L2P_Prob_prompt \
+    --model L2P_Prob_prompt_only_global_no_print \
+    --algorithm multimodal.food101_classification_8_classes.L2P_Prob_prompt_only_global \
     --sample full \
     --aggregate other \
     --num_rounds 250 \
     --proportion 1.0 \
     --lr_scheduler 0 \
-    --seed 5678 \
+    --seed 91011 \
     --fedmsplit_prox_lambda 0 \
     --learning_rate 0.05 \
     --num_epochs 1 \
     --num_outer_loops 5 \
     --learning_rate_decay 1.0 \
-    --note size_20_topk_5x2_back_to_101cls \
+    --note fixbug \
     --batch_size 512 \
     --test_batch_size 512 \
     --max_text_len 40 \
@@ -40,34 +41,14 @@ python generate_fedtask.py \
     --missing \
     --missing_ratio_train 0.7 \
     --missing_ratio_test 0.7 \
-    --missing_type_train both \
-    --missing_type_test both \
-    --both_ratio 0.5 \
-    --max_text_len 40
+    --missing_type_train image \
+    --missing_type_test image \
+    --both_ratio 0 \
+    --max_text_len 64
 python main.py \
-    --task food101_classification_8_classes_cnum20_dist0_skew0_seed0_missing_ratio_0.7_0.7_missing_type_both_both_both_ratio_0.5 \
-    --model L2P \
-    --algorithm multimodal.food101_classification_8_classes.L2P \
-    --sample full \
-    --aggregate other \
-    --num_rounds 250 \
-    --proportion 1.0 \
-    --lr_scheduler 0 \
-    --seed 91011 \
-    --fedmsplit_prox_lambda 0 \
-    --learning_rate 0.05 \
-    --num_epochs 1 \
-    --learning_rate_decay 1.0 \
-    --note loadvilt \
-    --batch_size 512 \
-    --test_batch_size 512 \
-    --max_text_len 40 \
-    --gpu 0 \
-    --wandb
-python main.py \
-    --task food101_classification_8_classes_cnum20_dist0_skew0_seed0_missing_ratio_0.7_0.7_missing_type_both_both_both_ratio_0.5 \
-    --model Prob_prompt \
-    --algorithm multimodal.food101_classification_8_classes.Prob_prompt \
+    --task food101_classification_8_classes_cnum20_dist0_skew0_seed0_missing_ratio_0.7_0.7_missing_type_image_image_both_ratio_0.0 \
+    --model L2P_Prob_prompt_only_global \
+    --algorithm multimodal.food101_classification_8_classes.L2P_Prob_prompt_only_global \
     --sample full \
     --aggregate other \
     --num_rounds 250 \
@@ -79,7 +60,7 @@ python main.py \
     --num_epochs 1 \
     --num_outer_loops 5 \
     --learning_rate_decay 1.0 \
-    --note loadvilt \
+    --note fixbug \
     --batch_size 512 \
     --test_batch_size 512 \
     --max_text_len 40 \
