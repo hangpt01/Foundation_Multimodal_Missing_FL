9160c622369ddad64693ae4d1d14785ea99ecb22
Hang Phung <hang.phungthu01@gmail.com>
2024-11-11 08:32:07 +0000

    Adding scripts - motivation

---
+++ script/motivation/0.25/proposal.sh
@@ -0,0 +1,21 @@
+python main.py \
+    --task food101_motivation_cnum20_dist0_skew0_seed0_missing_ratio_0.25_0.25_missing_type_both_both_both_ratio_0.5 \
+    --model L2P_Prob_prompt_only_global_no_print \
+    --algorithm multimodal.food101_motivation.L2P_Prob_prompt_only_global \
+    --sample full \
+    --aggregate other \
+    --num_rounds 150 \
+    --proportion 1.0 \
+    --lr_scheduler 0 \
+    --seed 1234 \
+    --fedmsplit_prox_lambda 0 \
+    --learning_rate 0.05 \
+    --num_epochs 1 \
+    --num_outer_loops 5 \
+    --learning_rate_decay 1.0 \
+    --note fix_bug \
+    --batch_size 128 \
+    --test_batch_size 128 \
+    --max_text_len 40 \
+    --gpu 3 \
+    --wandb
---
+++ script/motivation/1.0/missing_data_20.sh
@@ -0,0 +1,21 @@
+python main.py \
+    --task food101_motivation_cnum20_dist0_skew0_seed0_missing_ratio_1.0_1.0_missing_type_both_both_both_ratio_0.5 \
+    --model get_missing_data \
+    --algorithm multimodal.food101_motivation.get_missing_data \
+    --sample full \
+    --aggregate other \
+    --num_rounds 250 \
+    --proportion 1.0 \
+    --lr_scheduler 0 \
+    --seed 1234 \
+    --fedmsplit_prox_lambda 0 \
+    --learning_rate 0.01 \
+    --num_epochs 1 \
+    --learning_rate_decay 1.0 \
+    --note loadvilt \
+    --batch_size 256 \
+    --test_batch_size 256 \
+    --max_text_len 40 \
+    --gpu 1 
+    # \
+    # --wandb
