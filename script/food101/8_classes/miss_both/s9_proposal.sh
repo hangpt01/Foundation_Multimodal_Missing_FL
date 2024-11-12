ae9fe772388721ee4cbb6f712367adb376253c91
Hang Phung <hang.phungthu01@gmail.com>
2024-11-12 08:46:07 +0700

    Add script Food 50GB

---
+++ script/food101/8_classes/miss_both/s9_proposal.sh
@@ -0,0 +1,21 @@
+python main.py \
+    --task food101_classification_8_classes_cnum20_dist0_skew0_seed0_missing_ratio_0.7_0.7_missing_type_both_both_both_ratio_0.5 \
+    --model L2P_Prob_prompt_only_global_no_print \
+    --algorithm multimodal.food101_classification_8_classes.L2P_Prob_prompt_only_global \
+    --sample full \
+    --aggregate other \
+    --num_rounds 250 \
+    --proportion 1.0 \
+    --lr_scheduler 0 \
+    --seed 91011 \
+    --fedmsplit_prox_lambda 0 \
+    --learning_rate 0.05 \
+    --num_epochs 1 \
+    --num_outer_loops 5 \
+    --learning_rate_decay 1.0 \
+    --note fix_bug \
+    --batch_size 512 \
+    --test_batch_size 512 \
+    --max_text_len 40 \
+    --gpu 0 \
+    --wandb
