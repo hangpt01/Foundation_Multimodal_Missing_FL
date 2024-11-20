2f78b88bf737b38229ce1b842113041b1ecd84a6
Hang Phung <hang.phungthu01@gmail.com>
2024-11-20 23:38:58 +0700

    Add script ablation pool size 0.75

---
+++ script/food101/ablation_pool_size_0.75/all_pools_except_20.sh
@@ -0,0 +1,84 @@
+python main.py \
+    --task food101_motivation_cnum20_dist0_skew0_seed0_missing_ratio_0.7_0.7_missing_type_both_both_both_ratio_0.5 \
+    --model proposal_pool_10 \
+    --algorithm multimodal.food101_motivation.L2P_Prob_prompt_only_global \
+    --sample full \
+    --aggregate other \
+    --num_rounds 150 \
+    --proportion 1.0 \
+    --lr_scheduler 0 \
+    --seed 1234 \
+    --fedmsplit_prox_lambda 0 \
+    --learning_rate 0.05 \
+    --num_epochs 1 \
+    --num_outer_loops 5 \
+    --learning_rate_decay 1.0 \
+    --note ablation_pool \
+    --batch_size 128 \
+    --test_batch_size 128 \
+    --max_text_len 40 \
+    --gpu 0 \
+    --wandb
+python main.py \
+    --task food101_motivation_cnum20_dist0_skew0_seed0_missing_ratio_0.7_0.7_missing_type_both_both_both_ratio_0.5 \
+    --model proposal_pool_15 \
+    --algorithm multimodal.food101_motivation.L2P_Prob_prompt_only_global \
+    --sample full \
+    --aggregate other \
+    --num_rounds 150 \
+    --proportion 1.0 \
+    --lr_scheduler 0 \
+    --seed 1234 \
+    --fedmsplit_prox_lambda 0 \
+    --learning_rate 0.05 \
+    --num_epochs 1 \
+    --num_outer_loops 5 \
+    --learning_rate_decay 1.0 \
+    --note ablation_pool \
+    --batch_size 128 \
+    --test_batch_size 128 \
+    --max_text_len 40 \
+    --gpu 0 \
+    --wandb
+python main.py \
+    --task food101_motivation_cnum20_dist0_skew0_seed0_missing_ratio_0.7_0.7_missing_type_both_both_both_ratio_0.5 \
+    --model proposal_pool_25 \
+    --algorithm multimodal.food101_motivation.L2P_Prob_prompt_only_global \
+    --sample full \
+    --aggregate other \
+    --num_rounds 150 \
+    --proportion 1.0 \
+    --lr_scheduler 0 \
+    --seed 1234 \
+    --fedmsplit_prox_lambda 0 \
+    --learning_rate 0.05 \
+    --num_epochs 1 \
+    --num_outer_loops 5 \
+    --learning_rate_decay 1.0 \
+    --note ablation_pool \
+    --batch_size 128 \
+    --test_batch_size 128 \
+    --max_text_len 40 \
+    --gpu 0 \
+    --wandb
+python main.py \
+    --task food101_motivation_cnum20_dist0_skew0_seed0_missing_ratio_0.7_0.7_missing_type_both_both_both_ratio_0.5 \
+    --model proposal_pool_30 \
+    --algorithm multimodal.food101_motivation.L2P_Prob_prompt_only_global \
+    --sample full \
+    --aggregate other \
+    --num_rounds 150 \
+    --proportion 1.0 \
+    --lr_scheduler 0 \
+    --seed 1234 \
+    --fedmsplit_prox_lambda 0 \
+    --learning_rate 0.05 \
+    --num_epochs 1 \
+    --num_outer_loops 5 \
+    --learning_rate_decay 1.0 \
+    --note ablation_pool \
+    --batch_size 128 \
+    --test_batch_size 128 \
+    --max_text_len 40 \
+    --gpu 0 \
+    --wandb
