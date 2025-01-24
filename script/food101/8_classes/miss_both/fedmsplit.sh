5da6d7040d05539c17018fa76e844ec0baf7f6f2
Hang Phung <hang.phungthu01@gmail.com>
2025-01-25 01:26:41 +0700

    Add scripts Fedmsplit

--- script/food101/8_classes/miss_both/fedmsplit.sh
+++ script/food101/8_classes/miss_both/fedmsplit.sh
@@ -1,3 +1,16 @@
+python generate_fedtask.py \
+    --benchmark food101_classification_8_classes \
+    --dist 0 \
+    --skew 0 \
+    --num_clients 20 \
+    --seed 0 \
+    --missing \
+    --missing_ratio_train 0.7 \
+    --missing_ratio_test 0.7 \
+    --missing_type_train both \
+    --missing_type_test both \
+    --both_ratio 0.5 \
+    --max_text_len 40
 python main.py \
     --task food101_classification_8_classes_cnum20_dist0_skew0_seed0_missing_ratio_0.7_0.7_missing_type_both_both_both_ratio_0.5 \
     --model fedmsplit \
@@ -13,9 +26,8 @@ python main.py \
     --num_epochs 1 \
     --learning_rate_decay 1.0 \
     --note loadvilt \
-    --batch_size 128 \
-    --test_batch_size 128 \
+    --batch_size 512 \
+    --test_batch_size 512 \
     --max_text_len 40 \
-    --gpu 0 
-    # \
-    # --wandb
+    --gpu 0 \
+    --wandb
