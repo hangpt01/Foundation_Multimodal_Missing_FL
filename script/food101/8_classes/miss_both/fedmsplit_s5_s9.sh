d137a0aa50517156096d49563dbdf7a9c4f21bdc
Hang Phung <hang.phungthu01@gmail.com>
2025-03-03 15:52:13 +0700

    Add scripts Fedmsplit

--- script/food101/8_classes/miss_both/fedmsplit_s5_s9.sh
+++ script/food101/8_classes/miss_both/fedmsplit_s5_s9.sh
@@ -31,3 +31,23 @@ python main.py \
     --max_text_len 40 \
     --gpu 0 \
     --wandb
+python main.py \
+    --task food101_classification_8_classes_cnum20_dist0_skew0_seed0_missing_ratio_0.7_0.7_missing_type_both_both_both_ratio_0.5 \
+    --model fedmsplit \
+    --algorithm multimodal.food101_classification_8_classes.fedmsplit \
+    --sample full \
+    --aggregate other \
+    --num_rounds 250 \
+    --proportion 1.0 \
+    --lr_scheduler 0 \
+    --seed 91011 \
+    --fedmsplit_prox_lambda 0.01 \
+    --learning_rate 0.05 \
+    --num_epochs 1 \
+    --learning_rate_decay 1.0 \
+    --note loadvilt \
+    --batch_size 512 \
+    --test_batch_size 512 \
+    --max_text_len 40 \
+    --gpu 0 \
+    --wandb
