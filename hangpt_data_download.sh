724b4b5f7f3a0049d10479ad8aac33afe4b180b5
Hang Phung <hang.phungthu01@gmail.com>
2025-03-04 14:31:16 +0700

    Add scripts Fedmsplit

--- hangpt_data_download.sh
+++ hangpt_data_download.sh
@@ -5,6 +5,7 @@ pip install kaggle
 kaggle datasets download -d gianmarco96/upmcfood101
 pip install torch==2.3.1+cu118 torchvision==0.18.1+cu118 torchaudio==2.3.1+cu118 -f https://download.pytorch.org/whl/cu118/torch_stable.html
 
+# pip install torch==2.0.1+cu117 torchvision==0.15.2+cu117 torchaudio==2.0.1+cu117 -f https://download.pytorch.org/whl/cu117/torch_stable.html
 # pip install torch==1.11.0+cu113 torchvision==0.12.0+cu113 torchaudio===0.11.0+cu113 -f https://download.pytorch.org/whl/cu113/torch_stable.html
 
 # MM-IMDB
