9157abd2b141b7eca8d42dc71eb336aee1c69aab
Hang Phung <hang.phungthu01@gmail.com>
2025-03-04 13:35:40 +0700

    Add scripts Fedmsplit

--- script/food101/8_classes/miss_text/fedmsplit_s5_s9.sh
+++ script/food101/8_classes/miss_text/fedmsplit_s5_s9.sh
@@ -1,16 +1,16 @@
-python generate_fedtask.py \
-    --benchmark food101_classification_8_classes \
-    --dist 0 \
-    --skew 0 \
-    --num_clients 20 \
-    --seed 0 \
-    --missing \
-    --missing_ratio_train 0.7 \
-    --missing_ratio_test 0.7 \
-    --missing_type_train text \
-    --missing_type_test text \
-    --both_ratio 0 \
-    --max_text_len 40
+# python generate_fedtask.py \
+#     --benchmark food101_classification_8_classes \
+#     --dist 0 \
+#     --skew 0 \
+#     --num_clients 20 \
+#     --seed 0 \
+#     --missing \
+#     --missing_ratio_train 0.7 \
+#     --missing_ratio_test 0.7 \
+#     --missing_type_train text \
+#     --missing_type_test text \
+#     --both_ratio 0 \
+#     --max_text_len 40
 python main.py \
     --task food101_classification_8_classes_cnum20_dist0_skew0_seed0_missing_ratio_0.7_0.7_missing_type_text_text_both_ratio_0.0 \
     --model fedmsplit \
@@ -26,28 +26,28 @@ python main.py \
     --num_epochs 1 \
     --learning_rate_decay 1.0 \
     --note loadvilt \
-    --batch_size 512 \
-    --test_batch_size 512 \
+    --batch_size 256 \
+    --test_batch_size 256 \
     --max_text_len 40 \
     --gpu 0 \
     --wandb
-python main.py \
-    --task food101_classification_8_classes_cnum20_dist0_skew0_seed0_missing_ratio_0.7_0.7_missing_type_text_text_both_ratio_0.0 \
-    --model fedmsplit \
-    --algorithm multimodal.food101_classification_8_classes.fedmsplit \
-    --sample full \
-    --aggregate other \
-    --num_rounds 250 \
-    --proportion 1.0 \
-    --lr_scheduler 0 \
-    --seed 91011 \
-    --fedmsplit_prox_lambda 0.01 \
-    --learning_rate 0.05 \
-    --num_epochs 1 \
-    --learning_rate_decay 1.0 \
-    --note loadvilt \
-    --batch_size 512 \
-    --test_batch_size 512 \
-    --max_text_len 40 \
-    --gpu 0 \
-    --wandb
+# python main.py \
+#     --task food101_classification_8_classes_cnum20_dist0_skew0_seed0_missing_ratio_0.7_0.7_missing_type_text_text_both_ratio_0.0 \
+#     --model fedmsplit \
+#     --algorithm multimodal.food101_classification_8_classes.fedmsplit \
+#     --sample full \
+#     --aggregate other \
+#     --num_rounds 250 \
+#     --proportion 1.0 \
+#     --lr_scheduler 0 \
+#     --seed 91011 \
+#     --fedmsplit_prox_lambda 0.01 \
+#     --learning_rate 0.05 \
+#     --num_epochs 1 \
+#     --learning_rate_decay 1.0 \
+#     --note loadvilt \
+#     --batch_size 256 \
+#     --test_batch_size 256 \
+#     --max_text_len 40 \
+#     --gpu 0 \
+#     --wandb
---
+++ script/imdb/miss_both/fedmsplit_s9.sh
@@ -0,0 +1,33 @@
+python generate_fedtask.py \
+    --benchmark imdb_classification \
+    --dist 0 \
+    --skew 0 \
+    --num_clients 20 \
+    --seed 0 \
+    --missing \
+    --missing_ratio_train 0.7 \
+    --missing_ratio_test 0.7 \
+    --missing_type_train both \
+    --missing_type_test both \
+    --both_ratio 0.5 \
+    --max_text_len 128
+python main.py \
+    --task imdb_classification_cnum20_dist0_skew0_seed0_missing_ratio_0.7_0.7_missing_type_both_both_both_ratio_0.5 \
+    --model fedmsplit \
+    --algorithm multimodal.imdb_classification.fedmsplit \
+    --sample full \
+    --aggregate other \
+    --num_rounds 250 \
+    --proportion 1.0 \
+    --lr_scheduler 0 \
+    --seed 91011 \
+    --fedmsplit_prox_lambda 0.01 \
+    --learning_rate 0.01 \
+    --num_epochs 1 \
+    --learning_rate_decay 1.0 \
+    --note loadvilt \
+    --batch_size 256 \
+    --test_batch_size 256 \
+    --max_text_len 128 \
+    --gpu 0 \
+    --wandb
