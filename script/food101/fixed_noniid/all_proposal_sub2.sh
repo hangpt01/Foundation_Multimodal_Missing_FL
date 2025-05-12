ea60b234c8c06671a60c8625834bfa4c0765d722
Hang Phung <hang.phungthu01@gmail.com>
2025-05-12 16:06:01 +0700

    Add scripts

---
+++ script/food101/fixed_noniid/all_proposal_sub2.sh
@@ -0,0 +1,35 @@
+python generate_fedtask.py \
+    --benchmark food101_8_classes_fixed \
+    --dist 1 \
+    --skew 0.1 \
+    --num_clients 20 \
+    --seed 0 \
+    --missing \
+    --missing_ratio_train 0.7 \
+    --missing_ratio_test 0.7 \
+    --missing_type_train image \
+    --missing_type_test image \
+    --both_ratio 0 \
+    --max_text_len 40
+python main.py \
+    --task food101_8_classes_fixed_cnum20_dist1_skew0.1_seed0_missing_ratio_0.7_0.7_missing_type_image_image_both_ratio_0.0 \
+    --model proposal \
+    --algorithm multimodal.food101_8_classes_fixed.proposal \
+    --sample full \
+    --aggregate other \
+    --num_rounds 250 \
+    --proportion 1.0 \
+    --lr_scheduler 0 \
+    --seed 1234 \
+    --fedmsplit_prox_lambda 0 \
+    --learning_rate 0.05 \
+    --reduce_sim_scalar 0.005 \
+    --num_epochs 1 \
+    --num_outer_loops 5 \
+    --learning_rate_decay 1.0 \
+    --note rebuttal \
+    --batch_size 128 \
+    --test_batch_size 128 \
+    --max_text_len 40 \
+    --gpu 1 \
+    --wandb
