8b7986772136e3f6e36e564078b8e7461c42493b
Hang Phung <hang.phungthu01@gmail.com>
2025-05-12 15:19:54 +0700

    Add fedtask and 200clients setting for large server

---
+++ script/food101/large_scale_FL/proposal_200.sh
@@ -0,0 +1,35 @@
+python generate_fedtask.py \
+    --benchmark food101_8_classes_fixed \
+    --dist 0 \
+    --skew 0 \
+    --num_clients 200 \
+    --seed 0 \
+    --missing \
+    --missing_ratio_train 0.7 \
+    --missing_ratio_test 0.7 \
+    --missing_type_train both \
+    --missing_type_test both \
+    --both_ratio 0.5 \
+    --max_text_len 40
+python main.py \
+    --task food101_8_classes_fixed_cnum200_dist0_skew0_seed0_missing_ratio_0.7_0.7_missing_type_both_both_both_ratio_0.5 \
+    --model proposal \
+    --algorithm multimodal.food101_8_classes_fixed.proposal \
+    --sample uniform \
+    --aggregate other \
+    --num_rounds 250 \
+    --proportion 0.2 \
+    --lr_scheduler 0 \
+    --seed 1234 \
+    --fedmsplit_prox_lambda 0 \
+    --learning_rate 0.05 \
+    --reduce_sim_scalar 0.02 \
+    --num_epochs 1 \
+    --num_outer_loops 5 \
+    --learning_rate_decay 1.0 \
+    --note fix_bug \
+    --batch_size 128 \
+    --test_batch_size 128 \
+    --max_text_len 40 \
+    --gpu 0 \
+    --wandb
