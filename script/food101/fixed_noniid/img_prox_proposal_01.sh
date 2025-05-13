b27abcafaaa00ebd4d66d92365719a7db0be9d87
Hang Phung <hang.phungthu01@gmail.com>
2025-05-13 20:19:53 +0700

    Add NonIID scripts

--- script/food101/fixed_noniid/img_prox_proposal_01.sh
+++ script/food101/fixed_noniid/img_prox_proposal_01.sh
@@ -20,7 +20,7 @@ python main.py \
     --num_rounds 250 \
     --proportion 1.0 \
     --lr_scheduler 0 \
-    --seed 1234 \
+    --seed 5678 \
     --fedprox_lambda 0.01 \
     --learning_rate 0.05 \
     --reduce_sim_scalar 0.005 \
