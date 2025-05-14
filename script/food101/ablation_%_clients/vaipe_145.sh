e28d9c15d58b8f225d4a435d85e179ef42b95f3b
Hang Phung <hang.phungthu01@gmail.com>
2025-05-14 10:46:08 +0700

    Add NonIID scripts

---
+++ script/food101/ablation_%_clients/vaipe_145.sh
@@ -0,0 +1,63 @@
+python main.py \
+    --task food101_motivation_cnum100_dist0_skew0_seed0_missing_ratio_0.5_0.5_missing_type_text_text_both_ratio_0.0 \
+    --model fedmsplit \
+    --algorithm multimodal.food101_motivation.fedmsplit \
+    --sample uniform \
+    --aggregate other \
+    --num_rounds 250 \
+    --proportion 0.2 \
+    --lr_scheduler 0 \
+    --seed 5678 \
+    --fedmsplit_prox_lambda 0 \
+    --learning_rate 0.05 \
+    --num_epochs 1 \
+    --num_outer_loops 5 \
+    --learning_rate_decay 1.0 \
+    --note change_percentage_client \
+    --batch_size 512 \
+    --test_batch_size 512 \
+    --max_text_len 40 \
+    --gpu 0 \
+    --wandb
+python main.py \
+    --task food101_motivation_cnum100_dist0_skew0_seed0_missing_ratio_0.5_0.5_missing_type_text_text_both_ratio_0.0 \
+    --model L2P_Prob_prompt_only_global_no_print \
+    --algorithm multimodal.food101_motivation.L2P_Prob_prompt_only_global \
+    --sample uniform \
+    --aggregate other \
+    --num_rounds 250 \
+    --proportion 0.3 \
+    --lr_scheduler 0 \
+    --seed 5678 \
+    --fedmsplit_prox_lambda 0 \
+    --learning_rate 0.05 \
+    --num_epochs 1 \
+    --num_outer_loops 5 \
+    --learning_rate_decay 1.0 \
+    --note change_percentage_client \
+    --batch_size 512 \
+    --test_batch_size 512 \
+    --max_text_len 40 \
+    --gpu 0 \
+    --wandb
+python main.py \
+    --task food101_motivation_cnum100_dist0_skew0_seed0_missing_ratio_0.5_0.5_missing_type_text_text_both_ratio_0.0 \
+    --model L2P_Prob_prompt_only_global_no_print \
+    --algorithm multimodal.food101_motivation.L2P_Prob_prompt_only_global \
+    --sample uniform \
+    --aggregate other \
+    --num_rounds 250 \
+    --proportion 0.5 \
+    --lr_scheduler 0 \
+    --seed 5678 \
+    --fedmsplit_prox_lambda 0 \
+    --learning_rate 0.05 \
+    --num_epochs 1 \
+    --num_outer_loops 5 \
+    --learning_rate_decay 1.0 \
+    --note change_percentage_client \
+    --batch_size 512 \
+    --test_batch_size 512 \
+    --max_text_len 40 \
+    --gpu 0 \
+    --wandb
